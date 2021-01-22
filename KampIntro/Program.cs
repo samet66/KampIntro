faa209fbe08881ccc2b48f925efb4790244cdd5a
Samet Şahingöz <sametsahingoz@outlook.com>
2021-01-22 21:00:16 +0300

    16Ocak ders sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool SistemeGieis = false;
+
+            double dolarDun = 7.05;
+            double dolarBugn = 7.50;
+
+            if (dolarDun > dolarBugn)
+            {
+                Console.WriteLine("Dolar düştü");
+            }
+            else if (dolarDun < dolarBugn)
+            {
+                Console.WriteLine("Dolar yüksek");
+            }
+            else
+            {
+                Console.WriteLine("aynı");
+            }
+
+
+            if (SistemeGieis == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urunler urun1 = new Urunler
+            {
+                Id = 1,
+                UrunAdi = "Elma",
+                Fiyat = 2.5,
+                Aciklama = "1kg Köy elması"
+            };
+
+            Urunler urun2 = new Urunler
+            {
+                Id = 2,
+                UrunAdi = "Karpuz",
+                Fiyat = 56.5,
+                Aciklama = "1KG karpuz"
+            };
+
+            Urunler urun3 = new Urunler
+            {
+                Id = 3,
+                UrunAdi = "Muz",
+                Fiyat = 12,
+                Aciklama = "Yerli muz"
+            };
+
+
+            Urunler[] urun = new Urunler[]
+            {
+                urun1,urun2,urun3
+            };
+            
+
+            foreach (Urunler item in urun)
+            {
+                Console.WriteLine(
+                    item.UrunAdi + "\n" +
+                    item.Fiyat + "\n" +
+                    item.Aciklama + "\n......................"
+                    );
+            }
+
+
+            Console.WriteLine("\n\n\n.................................Metotlar.............................");
+
+            SepetIslemleri sepet = new SepetIslemleri();
+            sepet.Ekle(urun1);
+            sepet.Ekle(urun2);
+            sepet.Ekle(urun3);
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
